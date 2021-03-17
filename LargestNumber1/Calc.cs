00ff2a551721b201c9ac9dfbada17c4a4599cf7c
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:44:48 +0100

    Initial commit

---
+++ LargestNumber1/Calc.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections.Generic;
+
+namespace LargestNumber1
+{
+    public class Calc
+    {
+        private readonly int[][] _arrArr;
+        private readonly List<int> _intList;
+
+        public Calc(int[][] arrArr)
+        {
+            _arrArr = arrArr;
+            _intList = new List<int>();
+        }
+
+        public int[] Calc1()
+        {
+            foreach (var arr in _arrArr)
+            {
+                int? x = null;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    int j = arr[i];
+                    if (!x.HasValue || j > x.Value) x = j;
+                }
+                _intList.Add((int)x);
+            }
+            return _intList.ToArray();
+        }
+    }
+}
---
+++ LargestNumber1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LargestNumber1
+{
+    class Program
+    {
+        // https://edabit.com/challenge/nermqxzovZbfFBC9X
+        // SOLVED
+        // He don't need no LINQ, nonoooo
+
+        static void Main(string[] args)
+        {
+            var arr1 = new int[] { 4, 2, -7, 1 };
+            var arr2 = new int[] { 20, 70, 40, 90, 100, 200, 500, -900 };
+            var arr3 = new int[] { 1, 2, 0 };
+            var arrArr = new Calc(new int[][] { arr1, arr2, arr3 });
+            var arr = arrArr.Calc1();
+            foreach (var x in arr) Console.WriteLine(x);
+        }
+    }
+}
